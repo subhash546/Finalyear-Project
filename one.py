ea21cb9accfda4d6dde4fceb6d6ca05bef3a990f
Subhash546 <subhashrowtu3@gmail.com>
2022-12-26 21:30:07 +0530

    after db connect

--- one.py
+++ one.py
@@ -1,6 +1,6 @@
 print("Welcome")
 print("Subhash")
-from flask import Flask, render_template
+from flask import Flask,render_template,request
  
 app = Flask(__name__)
  
@@ -8,6 +8,23 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/login')  
+def login():  
+    return render_template("login.html");  
+
+@app.route('/reg')  
+def reg():  
+    return render_template("register.html");  
+
+
+@app.route('/', methods=['POST'])
+def result():
+    email=request.form['tf1']
+    password=request.form['tf2']
+   
+    return render_template('pass.html',n=email,c=password)
+ 
  
 if __name__ == "__main__":
     app.run()
