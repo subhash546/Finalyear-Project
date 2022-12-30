a483775231073f6f62bc2214ec9d3b344e944c1e
Subhash546 <subhashrowtu3@gmail.com>
2022-12-30 14:37:49 +0530

    modification

--- one.py
+++ one.py
@@ -1,6 +1,9 @@
 print("Welcome")
 print("Subhash")
-from flask import Flask,render_template,request
+
+import sqlite3
+from flask import Flask,render_template,url_for,request
+
  
 app = Flask(__name__)
  
@@ -9,22 +12,48 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/login')  
+@app.route('/login2')  
 def login():  
-    return render_template("login.html");  
+    return render_template("login2.html");  
 
 @app.route('/reg')  
 def reg():  
     return render_template("register.html");  
 
 
-@app.route('/', methods=['POST'])
-def result():
-    email=request.form['tf1']
-    password=request.form['tf2']
-   
-    return render_template('pass.html',n=email,c=password)
- 
- 
-if __name__ == "__main__":
-    app.run()
+    
+
+
+import sqlite3  
+  
+con = sqlite3.connect("my.db")  
+print("Database opened successfully")  
+  
+
+  
+  
+  
+
+@app.route("/savedetails",methods = ["POST","GET"])  
+def saveDetails():  
+    msg = "msg"  
+    if request.method == "POST":  
+        try:   
+            email = request.form["email"]  
+            password = request.form["password"] 
+            cpassword = request.form["cpassword"] 
+            with sqlite3.connect("my.db") as con:  
+                cur = con.cursor()  
+                cur.execute("INSERT into subhash ( email,password,cpassword) values (?,?,?)",(email,password,cpassword))  
+                con.commit()  
+                msg = "Employee successfully Added"
+               
+        except:  
+            con.rollback()  
+            msg = "We can not add the employee to the list"  
+        finally: 
+            print(msg); 
+            return render_template("success.html",msg = msg)  
+            con.close()  
+if __name__=='__main__':
+    app.run(debug=True)
