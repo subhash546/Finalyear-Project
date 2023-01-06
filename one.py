a05b1f2e297ee37021fb070b3f0792881c2809d5
Subhash546 <subhashrowtu3@gmail.com>
2023-01-06 07:32:34 +0530

    last change

--- one.py
+++ one.py
@@ -2,15 +2,34 @@ print("Welcome")
 print("Subhash")
 
 import sqlite3
-from flask import Flask,render_template,url_for,request
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
+
 
  
 app = Flask(__name__)
+app.secret_key = "hello"
  
  
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route('/abs')  
+def abs():  
+    return render_template("abstract.html");  
+
+@app.route('/team')  
+def team():  
+    return render_template("team.html"); 
+
+@app.route('/info')  
+def guide():    
+    return render_template("guide.html");
+@app.route('/dash')  
+def dash():  
+    return render_template("dashboard.html");  
 
 @app.route('/login2')  
 def login():  
@@ -19,6 +38,7 @@ def login():
 @app.route('/reg')  
 def reg():  
     return render_template("register.html");  
+
 
 
     
@@ -36,13 +56,13 @@ print("Database opened successfully")
 def savedetails():  
     msg = "demo"  
     if request.method == "POST":  
-        try:  
-            name = request.form["name"]  
+        try: 
             email = request.form["email"]  
-            password = request.form["password"]  
+            password = request.form["password"]
+            cPassword = request.form["cPassword"]   
             with sqlite3.connect("subhash.db") as con:  
                 cur = con.cursor()  
-                cur.execute("INSERT into pro (name, email, password) values (?,?,?)",(name,email,password))  
+                cur.execute("INSERT into pro ( email, password,cPassword) values (?,?,?)",(email,password,cPassword))  
                 con.commit()  
                 msg = "User successfully Added"  
         except:  
